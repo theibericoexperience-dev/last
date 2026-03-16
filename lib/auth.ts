eb98f21244cade5a48662108dc5574c219d212e1
theibericoexperience-dev <theibericoexperience@gmail.com>
2026-03-16 15:45:37 -0400

    fix: total cache purge and sync with production client ID

--- lib/auth.ts
+++ lib/auth.ts
@@ -25,7 +25,7 @@ const authConfig = NextAuth({
   }),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
     CredentialsProvider({
